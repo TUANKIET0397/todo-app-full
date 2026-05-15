40d06c3bd8a21d7039f2d0f17be215260f80083a
TUANKIET0397 <kiet8103@gmail.com>
2026-05-15 13:28:29 +0700

    update nextjs with lib antd, @anth-design/nextjs-registry for fix flick layout = design dashboarch admin ...

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -1,3 +1,15 @@
+
+import { Inter } from 'next/font/google'
+import { AntdRegistry } from '@ant-design/nextjs-registry';
+import '@/app/globals.css'
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'KietCoder',
+  description: 'A simple Next.js application with Ant Design',
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>
+        <AntdRegistry>
+          {children}
+        </AntdRegistry>
+      </body>
     </html>
   )
 }
--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from 'next/image'
+import { Button } from 'antd'
 
 export default function Page() {
   return (
     <>
         <h1>Hello, Next.js!</h1>
-        <Image src="/img.png" alt="Description of image" width={300} height={300} />
+
+        <div>
+          <Button type="primary" >Click me</Button>
+        </div>
+        <Image src="/img.png" loading="eager" alt="Description of image" width={300} height={300} />
     </>
   )
 }
